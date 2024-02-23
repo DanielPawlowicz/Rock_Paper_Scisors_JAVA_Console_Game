d667eca62c294581b3624a466fdab8a974c7f71b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-23 12:42:12 +0100

    Generating random computer's choice

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,36 @@ import java.util.Scanner;
 
 public class Main {
 
-	boolean gameStarted = false;
-	Random random;
+	static boolean gameStarted = false;
+	static Random random;
 	Scanner scanner;
 	String usersChoice;
-	String computersChoice;
+	static String computersChoice;
 	int usersPoints = 0;
 	int computersPoints = 0;
 	
 	
-	public void startGame() {
+	public static void startGame() {
+		gameStarted = true;
+//		System.out.println("Works");
+		
+		// Random computer's choice
+		random = new Random();
+		int rNumber = random.nextInt(3);
+		switch(rNumber) {
+			case 0:
+				computersChoice = "Rock";
+				break;
+			case 1:
+				computersChoice = "Paper";
+				break;
+			case 2:
+				computersChoice = "Scisors";
+				break;
+		}
 		
 		
+		// Asking user for 
 		
 	}
 	
@@ -33,7 +51,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		startGame();
 		
 	}
 
