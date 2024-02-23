92b1c71abc392c95141a45a19e38054a088f313e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-23 12:17:33 +0100

    Declaring classes, objects, variables and methods

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	boolean gameStarted = false;
+	Random random;
+	Scanner scanner;
+	String usersChoice;
+	String computersChoice;
+	int usersPoints = 0;
+	int computersPoints = 0;
+	
+	
+	public void startGame() {
+		
+		
+		
+	}
+	
+	public void checkWins() {
+		
+	}
+	
+	public void displayScore() {
+		
+	}
+	
+	public void nextRound() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
